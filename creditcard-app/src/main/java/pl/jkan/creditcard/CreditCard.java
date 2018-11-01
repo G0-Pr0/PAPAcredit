12f566b283c36c13e139f03ef50e6b0ced5026df
Mesiya391 <33184329+Mesiya391@users.noreply.github.com>
2018-11-01 17:00:38 +0100

    complete withdraw_feature

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -2,7 +2,15 @@ package pl.jkan.creditcard;
 
 class CreditCard {
     private double balance = 0;
+	private boolean blocked =false;
     private double cardlimit = 0;
+	public void block(){
+		this.blocked=true;
+	}
+	public boolean isBlocked(){
+		return blocked;
+	}
+	
     public void assignLimit(double limit) {
         balance = limit;
         cardlimit = limit;
